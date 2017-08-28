5be1a17dda1dceee3a1894e4ee75b7953ee4690f
Sambasiva <sivarunreddy@gmail.com>
2017-08-28 10:54:54 +0100

    Added CORS

--- src/main/java/context/hotel/HotelApplication.java
+++ src/main/java/context/hotel/HotelApplication.java
@@ -2,12 +2,26 @@ package context.hotel;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class HotelApplication {
 
-  public static void main(String[] args) {
-    SpringApplication.run(HotelApplication.class, args);
-  }
+    public static void main(String[] args) {
+        SpringApplication.run(HotelApplication.class, args);
+    }
 
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/search").allowedOrigins("http://localhost:8081, http://54.145.202.206:8080");
+            }
+        };
+    }
 }
